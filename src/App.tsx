79b44ae08cf33f76c893862ce7b97fe5952744e6
Franklin Padilla <fpadilla@buk.cl>
2024-01-12 22:42:49 -0400

    Update page title when changing months

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppLayout from './AppLayout'
+import useSelectedMonthStore from './stores/selectedMonthStore'
+import { format } from 'date-fns'
 
 function App (): React.ReactElement {
+  const selectedMonth = useSelectedMonthStore(store => store.selectedMonth)
+
+  useEffect(
+    () => {
+      document.title = `Google Calendar Clone - ${format(selectedMonth, 'MMM yyy')}`
+    },
+    [selectedMonth]
+  )
+
   return (
     <AppLayout />
   )
